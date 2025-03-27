4504de430e4009cc9dee55c9ee45975f7f5a0b65
sauravbhise.manastik <saurav.bhise@manastik.com>
2025-03-27 13:37:00 +0530

    Update La-Student Mapping endpoints

--- src/main/java/com/example/learningassistance/controller/LaStudentMappingController.java
+++ src/main/java/com/example/learningassistance/controller/LaStudentMappingController.java
@@ -1,7 +1,9 @@
 package com.example.learningassistance.controller;
 
 import com.example.learningassistance.model.LaStudentMapping;
+import com.example.learningassistance.model.User;
 import com.example.learningassistance.repo.LaStudentMappingRepo;
+import com.example.learningassistance.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,8 @@ public class LaStudentMappingController {
 
     @Autowired
     LaStudentMappingRepo laStudentMappingRepo;
+    @Autowired
+    UserRepo userRepo;
 
     @GetMapping("/la-students")
     public ResponseEntity<List<LaStudentMapping>> getAllMappings() {
@@ -45,8 +49,8 @@ public class LaStudentMappingController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @GetMapping("/la/{laId}/students")
-    public ResponseEntity<List<LaStudentMapping>> getMappingByLaId(@PathVariable long laId) {
+    @GetMapping("/la-students/{laId}/students/{studentId}")
+    public ResponseEntity<Optional<LaStudentMapping>> getMappingBylaIdandStudent(@PathVariable long laId, @PathVariable long studentId) {
         try {
             List<LaStudentMapping> mappingList = new ArrayList<>();
             laStudentMappingRepo.findByLaId(laId).forEach(mappingList::add);
@@ -55,7 +59,37 @@ public class LaStudentMappingController {
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
 
-            return new ResponseEntity<>(mappingList, HttpStatus.OK);
+            Optional<LaStudentMapping> studentMapping = mappingList.stream()
+                    .filter(mapping -> mapping.getStudentId() == studentId).findFirst();
+
+            if (studentMapping.isPresent()) {
+                return new ResponseEntity<>(studentMapping, HttpStatus.OK);
+            }
+
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/la/{laId}/students")
+    public ResponseEntity<List<Optional<User>>> getMappingByLaId(@PathVariable long laId) {
+        try {
+            List<LaStudentMapping> mappingList = new ArrayList<>();
+            laStudentMappingRepo.findByLaId(laId).forEach(mappingList::add);
+
+            if (mappingList.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            List<Optional<User>> studentList = new ArrayList<>();
+
+            mappingList.forEach(mapping -> {
+                studentList.add(userRepo.findById(mapping.getStudentId()));
+            });
+
+            return new ResponseEntity<>(studentList, HttpStatus.OK);
 
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -74,4 +108,21 @@ public class LaStudentMappingController {
         laStudentMappingRepo.deleteById(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    @DeleteMapping("/{laId}/la-students/{studentId}")
+    public ResponseEntity<HttpStatus> deleteStudentFromLa(@PathVariable long laId, @PathVariable long studentId) {
+        List<LaStudentMapping> allStudentsForLa = laStudentMappingRepo.findByLaId(laId);
+
+        Optional<LaStudentMapping> mappingToDelete = allStudentsForLa.stream()
+                .filter(mapping -> mapping.getStudentId() == studentId)
+                .findFirst();
+
+        if (mappingToDelete.isPresent()) {
+            laStudentMappingRepo.delete(mappingToDelete.get());
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
 }
